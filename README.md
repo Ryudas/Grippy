bb891c91f5097d2f019f38fb5074c636b40ae463
localadmin <sivlesegrob9@gmail.com>
2019-06-14 09:10:44 +0200

    Added maps initial app implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+# stress_manager
+
+Flutter project to help people afflicted with PTSD manage their stress
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // google maps API
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Map<String, Marker> _markers = { "one": Marker( markerId: MarkerId("one"),
+                                                        position: LatLng(52.011034,4.357725),
+                                                      )
+                                       };
+
+  GoogleMapController mapController;    // creating google map controler object
+
+  final LatLng _center = const LatLng(52.011578, 4.357068); // Latitude longitude
+  // class , type of object that represents a position in the world
+
+  // when map object is created
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('strain manager'),
+          backgroundColor: Colors.teal,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+
+          ),
+          markers:_markers.values.toSet(),
+        ),
+      ),
+    );
+  }
+}
