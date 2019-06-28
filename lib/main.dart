c8bf6443878e6766a263bc17d0fba5e83d408629
localadmin <sivlesegrob9@gmail.com>
2019-06-28 11:28:27 +0200

    Added Geolocator, and device position

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart'; // google maps API
-
+import 'package:geolocator/geolocator.dart'; // package for geolocation
 
 import 'markers.dart';
 
@@ -12,7 +14,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final Map<String, Marker> _markers =
+
+  // Map location markers
+  final Map<String, Marker> _markers = process_markers();
+
+  // current user location marker
+  Marker curr_location = Marker( markerId: MarkerId("loc"),
+                                 position: LatLng(double.parse(_loc_values[0]),
+                                                  double.parse(_loc_values[1])
+                                                  )
+                         );
+
+
+  // stores list of subscriptions to sensor event streams (async data sources)
+  List<StreamSubscription<dynamic>> _stream_subscriptions =
+  <StreamSubscription<dynamic>>[];
+
+  // saves location data, 5D:
+  // latitude in degrees normalized to the interval [-90.0,+90.0]
+  // longitude in degrees normalized to the interval [-90.0,+90.0]
+  // altitude in meters
+  // speed at which the device is traveling in m/s over ground
+  // timestamp time at which event was received from device
+  List<String> _loc_values;
 
   GoogleMapController mapController;    // creating google map controler object
 
@@ -22,6 +46,36 @@ class _MyAppState extends State<MyApp> {
   // when map object is created
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+
+  // registering our sensor stream subscriptions
+  // called when stateful widget is inserted in widget tree.
+  @override
+  void initState() {
+    super.initState(); // must be included
+
+    // Location subscription
+    var geolocator = Geolocator();
+    // desired accuracy and the minimum distance change
+    // (in meters) before updates are sent to the application - 1m in our case.
+    var location_options = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
+    _stream_subscriptions.add(
+        geolocator.getPositionStream(location_options).listen(
+                (Position event) {
+              setState(() {
+                _loc_values = <String>[event.latitude.toStringAsFixed(3),
+                  event.longitude.toStringAsFixed(3),
+                  event.altitude.toStringAsFixed(3),
+                  event.speed.toStringAsFixed(3),
+                  event.timestamp.toString()];
+              });
+
+            })
+    );
+
+
+
   }
 
   @override
@@ -39,7 +93,7 @@ class _MyAppState extends State<MyApp> {
             zoom: 15.0,
 
           ),
-          markers:_markers.values.toSet(),
+          markers:_markers.values.toSet().add(value)
         ),
       ),
     );
