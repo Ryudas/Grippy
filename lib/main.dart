3040f45f8a9f2674f69fe6d549476a628545160d
localadmin <sivlesegrob9@gmail.com>
2019-08-14 16:50:10 +0200

    Added data storage handler class

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,12 @@ import 'package:path_provider/path_provider.dart';  // library for indirect file
 
 import 'markers.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(storage: DataStorage()));
 
 class MyApp extends StatefulWidget {
+  MyApp({Key key, @required this.storage}) : super(key: key);
+
+  final DataStorage storage;
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -81,7 +84,6 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState(); // must be included
-
 
 
     BitmapDescriptor.fromAssetImage( ImageConfiguration(bundle: rootBundle),
@@ -329,6 +331,13 @@ class _MyAppState extends State<MyApp> {
        );
      }
 
+/*
+     // store messages
+     widget.storage.write_data(messages[0]).then( (File my_file){
+       debugPrint("${my_file.length()}");
+     });
+*/
+
      debugPrint("${messages}");
    }
 
@@ -343,16 +352,28 @@ class _MyAppState extends State<MyApp> {
 
    }
 
-   // Gets path of Documents directory for app
-   Future<String> get _localPath async {
-     final directory = await getApplicationDocumentsDirectory();
 
-     return directory.path;
-   }
 
-   Future<File> get _localFile async {
-     final path = await _localPath;
-     return File('$path/LOG.txt');
-   }
+}
 
+
+class DataStorage{
+  // Gets path of Documents directory for app
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/LOG.txt');
+  }
+
+  Future<File> write_data(String data) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$data', mode:FileMode.append); // don't truncate file
+  }
 }
